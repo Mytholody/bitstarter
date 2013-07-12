b870ff85d35a0dd86d1a07a30f729683f5f7e779
Ubuntu <ubuntu@ip-172-31-4-138.us-west-2.compute.internal>
2013-07-12 03:09:16 +0000

    Made changes to web.js

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// Reads index.html and allows printout from var printbuff
+var readin = fs.readFileSync('index.html','utf-8');
+var buffer = new Buffer(27);
+buffer.write(readin);
+var printbuff = buffer.toString('utf-8');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(printbuff);
 });
 
 var port = process.env.PORT || 5000;
